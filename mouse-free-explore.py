484e6a5291c59c4e0500adc8d1b2ee55072e2d63
gdev <gdeveloping0@gmail.com>
2023-10-06 12:12:21 +0800

    add functiuon: show detail window

--- mouse-free-explore.py
+++ mouse-free-explore.py
@@ -1,5 +1,5 @@
 
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import QApplication, QWidget, QDesktopWidget
 from PyQt5.QtGui import QPainter, QColor, QFont
 from PyQt5.QtCore import Qt, QThread, pyqtSignal
 import sys
@@ -11,23 +11,66 @@ import logging
 import threading
 from concurrent.futures import ThreadPoolExecutor
 import time
+import traceback
 
 
-TRANSPARENCY = 50  # 透明度
-CELL_WIDTH_COLUMN_SIZE = 75  # Cell Width
-CELL_HEIGHT_ROW_SIZE = 45  # Cell Height
-DELAY_HIDE_TIME = 5
-FONT_SIZE = 12  # 字体大小
-SHOW_INTERVAL = 5 * 1000  # 定时器间隔（毫秒 = 秒 * 1000）
-BACKGROUND_COLOR = (173, 216, 230)  # 背景：浅蓝色
+# customizable configuration
+# hotkey
 KEYBOARD_SHOW_HOTKEY = 'ctrl + alt + space'
 KEYBOARD_SHOW_DETAIL_HOTKEY = 'ctrl + alt + shift'
 KEYBOARD_HIDE_HOTKEY = 'esc'
 KEYBOARD_MOUSE_CLICK_LEFT_HOTKEY = 'ctrl + alt + enter'
 KEYBOARD_QUIT_HOTKEY = 'ctrl + alt + q'
-TOGGLE_SHOW_HOTKEY = {"CTRL", "ALT"}
+
+# log file path
 LOG_FILE_PATH = 'mouse-free.log'
-THREAD_POOL_MAX_WORKERS = 5
+# log level
+LOG_LEVEL = logging.INFO
+
+# screen
+# fixed configuration
+MAX_CELL_LEVEL = 2 # max level， start from 1
+
+# customizable configuration
+TRANSPARENCY = 50  # N / 100
+DELAY_HIDE_TIME = 5 # second
+BACKGROUND_COLOR = (173, 216, 230) 
+IDENTIFIER_FONT_COLOR = (0, 0, 0)
+THREAD_POOL_MAX_WORKERS = 5 
+
+# customizable configuration
+CELL_WIDTH_COLUMN_SIZE = 75  # level 1 cell width
+CELL_HEIGHT_ROW_SIZE = 45  # level 1 cell height
+FONT_SIZE = 12  # level 1 font size
+# fixed configuration
+IDENTIFIER_KEY_COUNT = 2
+
+# customizable configuration
+CELL_WIDTH_COLUMN_SIZE_LEVEL2 = 25 # level 2 cell width
+CELL_HEIGHT_ROW_SIZE_LEVEL2 = 15 # level 2 cell height
+FONT_SIZE_LEVEL2 = 9 # level 2 font size
+# fixed configuration
+IDENTIFIER_KEY_COUNT_LEVEL2 = 1
+CELL_COUNT_PER_WIDTH_PER_HEIGHT_LEVEL2 = 5
+SCREEN_WIDTH_COLUMN_SIZE_LEVEL2 = CELL_WIDTH_COLUMN_SIZE_LEVEL2 * CELL_COUNT_PER_WIDTH_PER_HEIGHT_LEVEL2
+SCREEN_HEIGHT_ROW_SIZE_LEVEL2 = CELL_HEIGHT_ROW_SIZE_LEVEL2 * CELL_COUNT_PER_WIDTH_PER_HEIGHT_LEVEL2
+
+
+# constant
+LEVEL_1 = "LEVEL_1"
+LEVEL_2 = "LEVEL_2"
+LEVEL_DEFAULT = LEVEL_1
+
+KEY_CELL_WIDTH_COLUMN_SIZE = "CELL_WIDTH_COLUMN_SIZE"
+KEY_CELL_HEIGHT_ROW_SIZE = "CELL_HEIGHT_ROW_SIZE"
+KEY_FONT_SIZE = "FONT_SIZE"
+KEY_SCREEN_WIDTH_COLUMN_SIZE = "SCREEN_WIDTH_COLUMN_SIZE"
+KEY_SCREEN_HEIGHT_ROW_SIZE = "SCREEN_HEIGHT_ROW_SIZE"
+KEY_IDENTIFIER_KEY_COUNT = "IDENTIFIER_KEY_COUNT"
+
+
+# logger
+logger = None
 
 
 def get_timestamp_ms():
@@ -49,14 +92,14 @@ def log_function_name_to_exit():
 def is_valid_single_alpha(key):
     return len(str(key)) == 1 and str(key).isalpha()
 
-def config_logger():
+def config_logger(log_level, log_file_path):
     logger = logging.getLogger()
 
-    logger.setLevel(logging.INFO)
+    logger.setLevel(log_level)
 
     # Create a console handler and a file handler
     console_handler = logging.StreamHandler()
-    file_handler = logging.FileHandler(LOG_FILE_PATH)
+    file_handler = logging.FileHandler(log_file_path)
 
     # Create a formatter and attach it to the handlers
     formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
@@ -71,7 +114,7 @@ def config_logger():
 
 
 # Create a logger
-logger = config_logger()
+logger = config_logger(LOG_LEVEL, LOG_FILE_PATH)
 
 
 class MyWindow(QWidget):
@@ -80,17 +123,43 @@ class MyWindow(QWidget):
 
         super(MyWindow, self).__init__()
 
+        self.desktop = QDesktopWidget()
+
         self.executor_pool = ThreadPoolExecutor(max_workers=THREAD_POOL_MAX_WORKERS)
 
-        self.init_ui()
+        self.screen_level = LEVEL_DEFAULT
+
+        self.x_start = 0
+        self.y_start = 0
 
         self.keys = []
 
-        # 监听键盘，独立线程
+        self.screen_size_config = {
+            LEVEL_1: {
+                KEY_CELL_WIDTH_COLUMN_SIZE: CELL_WIDTH_COLUMN_SIZE,
+                KEY_CELL_HEIGHT_ROW_SIZE: CELL_HEIGHT_ROW_SIZE,
+                KEY_FONT_SIZE: FONT_SIZE,
+                KEY_IDENTIFIER_KEY_COUNT: IDENTIFIER_KEY_COUNT
+            },
+            LEVEL_2: {
+                KEY_CELL_WIDTH_COLUMN_SIZE: CELL_WIDTH_COLUMN_SIZE_LEVEL2,
+                KEY_CELL_HEIGHT_ROW_SIZE: CELL_HEIGHT_ROW_SIZE_LEVEL2,
+                KEY_FONT_SIZE: FONT_SIZE_LEVEL2,
+                KEY_SCREEN_WIDTH_COLUMN_SIZE: SCREEN_WIDTH_COLUMN_SIZE_LEVEL2,
+                KEY_SCREEN_HEIGHT_ROW_SIZE: SCREEN_HEIGHT_ROW_SIZE_LEVEL2,
+                KEY_IDENTIFIER_KEY_COUNT: IDENTIFIER_KEY_COUNT_LEVEL2
+            }
+        }
+
+        logger.info('screen_size_config: {}'.format(self.screen_size_config))
+    
+        self.init_ui()
+
+        # listen keyboard
         self.keyboard_listener = KeyboardListener(logger)
         self.keyboard_listener.key_pressed_signal.connect(self.handle_keyboard)
         self.keyboard_listener.quit_signal.connect(self.quit_app)        
-        self.keyboard_listener.show_signal.connect(self.my_show)
+        self.keyboard_listener.show_signal.connect(self.my_show_full_window)
         self.keyboard_listener.show_detail_signal.connect(self.my_show_detail)
         self.keyboard_listener.hide_signal.connect(self.my_hide) 
         self.keyboard_listener.simulate_mouse_click_left_signal.connect(self.my_simulate_mouse_click_left)       
@@ -102,17 +171,20 @@ class MyWindow(QWidget):
     def init_ui(self):
         log_function_name_to_enter()
 
+        # 幕布透明度
         self.setWindowOpacity(TRANSPARENCY / 100)
+        # 隐藏边框，顶层显示
         self.setWindowFlags(Qt.FramelessWindowHint | Qt.WindowStaysOnTopHint)
+        # 隐藏窗口在任务栏的展示
+        self.setWindowFlags(Qt.Tool)
+
         p = self.palette()
         bg_color = QColor(*BACKGROUND_COLOR)
         p.setColor(self.backgroundRole(), bg_color)
-        self.setPalette(p)
+        self.setPalette(p) 
+
         self.showFullScreen()
-        # 隐藏窗口在任务栏的展示
-        self.setWindowFlags(Qt.Tool)
-        self.set_show_latest_time()
-        self.my_delayed_hide()
+        self.post_show_window()
 
         log_function_name_to_exit()
      
@@ -121,15 +193,52 @@ class MyWindow(QWidget):
         log_function_name_to_enter()
 
         try:
+            current_font_size = self.get_current_font_size()
+            current_cell_width_column_size = self.get_current_cell_width_column_size()
+            current_cell_height_row_size = self.get_current_cell_height_row_size()
+
+            current_screen_width_column_size = 0
+            current_screen_height_row_size = 0
+
+            # always full screen
+            self.setGeometry(0, 0, self.desktop.screenGeometry().width(), self.desktop.screenGeometry().height())
+
+            if self.screen_level == LEVEL_1:
+                current_screen_width_column_size = self.get_current_screen_width_column_size()
+                current_screen_height_row_size = self.get_current_screen_height_row_size()
+                self.x_start = 0
+                self.y_start = 0
+            else:
+                x, y = mouse.Controller().position
+                current_screen_width_column_size = self.get_current_screen_size_config()[KEY_SCREEN_WIDTH_COLUMN_SIZE]
+                current_screen_height_row_size = self.get_current_screen_size_config()[KEY_SCREEN_HEIGHT_ROW_SIZE]
+                self.x_start = max(0, x - current_screen_width_column_size // 2)
+                self.y_start = max(0, y - current_screen_height_row_size // 2)
+
+            logger.debug('availableGeometry: {}, {}'.format(self.desktop.availableGeometry().width(), self.desktop.availableGeometry().height()))
+            logger.debug('screenGeometry: {}, {}'.format(current_screen_width_column_size, current_screen_height_row_size))
+
+            logger.info('screen_level: {}; cell_width: {}; cell_height: {}; font_size: {}; window width: {}; window height: {}; x_start: {}; y_start: {}'.format(
+                self.screen_level, 
+                current_cell_width_column_size,
+                current_cell_height_row_size,
+                current_font_size,
+                self.width(),
+                self.height(),
+                self.x_start,
+                self.y_start
+            ))
+
             painter = QPainter()
             painter.begin(self)
-            painter.setFont(QFont('Arial', FONT_SIZE))       
-
-            identifier_count = 0
-            for width_column_idx in range(0, self.width(), CELL_WIDTH_COLUMN_SIZE):
-                for height_row_idx in range(0, self.height(), CELL_HEIGHT_ROW_SIZE):
-                    painter.drawText(width_column_idx + CELL_WIDTH_COLUMN_SIZE // 2 + FONT_SIZE, 
-                                     height_row_idx + CELL_HEIGHT_ROW_SIZE // 2 + FONT_SIZE, 
+            painter.setFont(QFont('Arial', current_font_size))
+            painter.setPen(QColor(*IDENTIFIER_FONT_COLOR))
+            
+            identifier_count = 0            
+            for width_column_idx in range(0, current_screen_width_column_size, current_cell_width_column_size):
+                for height_row_idx in range(0, current_screen_height_row_size, current_cell_height_row_size):
+                    painter.drawText(current_cell_width_column_size // 2 + width_column_idx + self.x_start, 
+                                     current_cell_height_row_size // 2 + height_row_idx + self.y_start, 
                                      str(self.get_cell_identifier(width_column_idx, height_row_idx)))
                     identifier_count += 1
             logger.info("identifier count: {}".format(identifier_count))
@@ -137,32 +246,94 @@ class MyWindow(QWidget):
             painter.end()
         except Exception as e:
             logger.error('paintEvent Exception: {}'.format(e.with_traceback))
+            traceback.print_exc()
         
         log_function_name_to_exit()
+
+
+    def get_current_screen_size_config(self):
+        return self.screen_size_config[self.screen_level]
+    
+
+    def get_current_screen_width_column_size(self):
+        if self.screen_level == LEVEL_1:
+            return self.desktop.screenGeometry().width()
+        else:
+            return self.get_current_screen_size_config()[KEY_SCREEN_WIDTH_COLUMN_SIZE]
+
+
+    def get_current_screen_height_row_size(self):
+        if self.screen_level == LEVEL_1:
+            return self.desktop.screenGeometry().height()
+        else:
+            return self.get_current_screen_size_config()[KEY_SCREEN_HEIGHT_ROW_SIZE]       
+
+
+    def get_current_font_size(self):
+        return self.get_current_screen_size_config()[KEY_FONT_SIZE]
+
+
+    def get_current_cell_width_column_size(self):
+        return self.get_current_screen_size_config()[KEY_CELL_WIDTH_COLUMN_SIZE]
+
+
+    def get_current_cell_height_row_size(self):
+        return self.get_current_screen_size_config()[KEY_CELL_HEIGHT_ROW_SIZE]
+
+
+    def get_current_identifier_key_count(self):
+        return self.get_current_screen_size_config()[KEY_IDENTIFIER_KEY_COUNT]
 
 
     def quit_app(self):
         log_function_name_to_enter()
 
-        QApplication.quit()
+        logger.info('quit sys')
+        self.hide()
+        sys.exit(0)
 
-        log_function_name_to_exit()
+        # log_function_name_to_exit()
 
     
     def handle_keyboard(self, e):
-        log_function_name_to_enter()
+        log_function_name_to_enter()        
 
-        del self.keys[0:-1]
+        identifier_key_count = self.get_current_identifier_key_count()
+        # len - x = count - 1
+        for _ in range(0, len(self.keys) - identifier_key_count + 1):
+            self.keys.pop(0)
         self.keys.append(str(e.name).upper())
-        if len(self.keys) == 2 and self.isVisible():
+        if len(self.keys) == identifier_key_count and self.isVisible():
             self.move_mouse()
 
         log_function_name_to_exit()
 
     
     def get_cell_identifier(self, column_width, row_height):
-        return string.ascii_uppercase[(row_height // CELL_HEIGHT_ROW_SIZE)] + string.ascii_uppercase[(column_width // CELL_WIDTH_COLUMN_SIZE)]
+        if self.screen_level == LEVEL_1:
+            return string.ascii_uppercase[row_height // self.get_current_cell_height_row_size()] +\
+                string.ascii_uppercase[column_width // self.get_current_cell_width_column_size()]
+        elif self.screen_level == LEVEL_2:
+            idx = (row_height // self.get_current_cell_height_row_size()) * CELL_COUNT_PER_WIDTH_PER_HEIGHT_LEVEL2 + (column_width // self.get_current_cell_width_column_size())
+            return string.ascii_uppercase[idx % len(string.ascii_uppercase)]
     
+
+    def get_target_mouse_position(self):
+        x_position = 0
+        y_position = 0
+        current_cell_height_row_size = self.get_current_cell_height_row_size()
+        current_cell_width_column_size = self.get_current_cell_width_column_size()
+        if self.screen_level == LEVEL_1:
+            height_row_key = str(self.keys[0]).upper()
+            width_column_key = str(self.keys[1]).upper()        
+            x_position = string.ascii_uppercase.index(width_column_key) * current_cell_width_column_size + current_cell_width_column_size // 2 + self.x_start
+            y_position = string.ascii_uppercase.index(height_row_key) * current_cell_height_row_size + current_cell_height_row_size // 2 + self.y_start
+        elif self.screen_level == LEVEL_2:
+            key_idx = string.ascii_uppercase.index(str(self.keys[0]).upper())
+            x_position = (key_idx % CELL_COUNT_PER_WIDTH_PER_HEIGHT_LEVEL2) * current_cell_width_column_size + current_cell_width_column_size // 2 + self.x_start
+            y_position = (key_idx // CELL_COUNT_PER_WIDTH_PER_HEIGHT_LEVEL2) * current_cell_height_row_size + current_cell_height_row_size // 2 + self.y_start
+        return x_position, y_position
+
 
     def is_match_hotkey(self, expect_key, actual_key):
         return len(expect_key.symmetric_difference(actual_key)) == 0
@@ -171,44 +342,41 @@ class MyWindow(QWidget):
     def move_mouse(self):
         log_function_name_to_enter()
 
-        if len(self.keys) != 2:
+        if len(self.keys) != self.get_current_identifier_key_count():
             logger.info('exit {}: not valid key count'.format(get_current_function_name()))
             return
         
-        height_row_key = str(self.keys[0]).upper()
-        width_column_key = str(self.keys[1]).upper()
-        logger.info('width_key {} is_valid_single_alpha(): {}'.format(width_column_key, is_valid_single_alpha(width_column_key)))
-        logger.info('hight_key {} is_valid_single_alpha(): {}'.format(height_row_key, is_valid_single_alpha(height_row_key)))
-        
-        if (not is_valid_single_alpha(width_column_key)) or (not is_valid_single_alpha(height_row_key)):
-            logger.info('exit {}: not valid alpha key'.format(get_current_function_name()))
+        if not self.isVisible():
+            logger.info('exit {}: window is not visible'.format(get_current_function_name()))
             return
         
-        logger.info(f'width column key: {width_column_key}; hight row key: {height_row_key}.')
-        x_position = string.ascii_uppercase.index(width_column_key) * CELL_WIDTH_COLUMN_SIZE + CELL_WIDTH_COLUMN_SIZE // 2
-        y_position = string.ascii_uppercase.index(height_row_key) * CELL_HEIGHT_ROW_SIZE + CELL_HEIGHT_ROW_SIZE // 2
-        logger.info(f"width column position: {x_position}; hight row position: {y_position}.")
+        logger.debug('pressed keys: {}'.format(self.keys))
+
+        for key in self.keys:
+            if not is_valid_single_alpha(key):
+                logger.info('exit {}: not valid alpha key'.format(get_current_function_name()))
+                return
+     
+        x_position, y_position = self.get_target_mouse_position()
+        logger.debug(f"width column position: {x_position}; hight row position: {y_position}.")
+
         with mouse.Controller() as controller:
             controller.position = (x_position, y_position)
+            logger.info('pressed keys: {}'.format(self.keys))
             logger.info('move mouse to ({}, {})'.format(x_position, y_position))   
+        
+        # after mouse position changed
         self.keys.clear()
-
         self.my_hide()
         
         log_function_name_to_exit()
 
 
-    def log_window_status(self, window):
-        logger.debug('window isWindow: {}'.format(window.isWindow()))
-        logger.debug('window isActiveWindow: {}'.format(window.isActiveWindow()))
-        logger.debug('window isFullScreen: {}'.format(window.isFullScreen()))
-        logger.debug('window isVisible: {}'.format(window.isVisible()))  
-
-
     def my_hide(self):
         log_function_name_to_enter()
-        self.log_window_status(self)
 
+        # 屏幕恢复默认级别
+        self.screen_level = LEVEL_DEFAULT
         self.hide()
         
         log_function_name_to_exit()
@@ -227,22 +395,42 @@ class MyWindow(QWidget):
 
 
     def my_delayed_hide(self):
+        log_function_name_to_enter()
+
         timer = threading.Timer(DELAY_HIDE_TIME, self.my_delayed_hide_task)
         timer.start()
         self.executor_pool.submit(timer)
+
+        log_function_name_to_exit()
 
 
     def set_show_latest_time(self):
         self.latest_show_timestamp = get_timestamp_ms()
 
 
-    def my_show(self):
-        log_function_name_to_enter()
-        self.log_window_status(self)
-
-        self.showFullScreen()
+    def post_show_window(self):
         self.set_show_latest_time()
         self.my_delayed_hide()
+
+
+    def my_show_full_window(self):
+        log_function_name_to_enter()
+
+        self.hide()
+        self.showFullScreen()
+        self.post_show_window()
+        
+        log_function_name_to_exit()
+
+    
+    def my_show_detail(self):
+        log_function_name_to_enter()
+        
+        self.screen_level = LEVEL_2
+
+        self.hide()
+        self.showFullScreen()
+        self.post_show_window()
         
         log_function_name_to_exit()
 
@@ -258,19 +446,7 @@ class MyWindow(QWidget):
 
         log_function_name_to_exit()
 
-    
-    def my_show_detail(self):
-        log_function_name_to_enter()
-        self.log_window_status(self)
 
-        x, y = mouse.Controller().position
-        DETAIL_CELL_LENGTH = 300
-        self.setGeometry(x - DETAIL_CELL_LENGTH // 2, y - DETAIL_CELL_LENGTH // 2, DETAIL_CELL_LENGTH, DETAIL_CELL_LENGTH)
-        self.show()
-        self.set_show_latest_time()
-        self.my_delayed_hide()
-        
-        log_function_name_to_exit()
 
 
 class KeyboardListener(QThread):
@@ -292,9 +468,8 @@ class KeyboardListener(QThread):
         keyboard.add_hotkey(KEYBOARD_HIDE_HOTKEY, self.async_hide_window)
         keyboard.add_hotkey(KEYBOARD_QUIT_HOTKEY, self.async_quit)
         keyboard.add_hotkey(KEYBOARD_MOUSE_CLICK_LEFT_HOTKEY, self.async_simulate_mouse_click_left)
-        
+     
 
-    
     def async_show_window(self):
         log_function_name_to_enter()
 
